db1c52a9ccfdaccb294f5f8af920624de86bf5f0
vartanbeno <vartanbeno@gmail.com>
2018-10-20 22:36:16 -0400

    use lists instead of dictionaries for the stats

--- classes/compression_table.py
+++ classes/compression_table.py
@@ -20,12 +20,13 @@ class CompressionTable:
         :param index: index used as the unfiltered version, which will be filtered using various methods.
         """
         self.index = index
+        self.terms = [term for term in index.keys()]
 
-        self.index_no_numbers = {}
-        self.index_case_folding = {}
-        self.index_remove_30_stopwords = {}
-        self.index_remove_150_stopwords = {}
-        self.index_stem = {}
+        self.terms_no_numbers = []
+        self.terms_case_folding = []
+        self.terms_remove_30_stopwords = []
+        self.terms_remove_150_stopwords = []
+        self.terms_stemmed = []
 
         self.stopwords = set(stopwords.words("english"))
         self.stopwords = random.sample(self.stopwords, 150)
@@ -54,7 +55,7 @@ class CompressionTable:
         Do nothing to index.
         :return: row featuring information on original index.
         """
-        return ["unfiltered", "{:,}".format(len(self.index)), "-", "-"]
+        return ["unfiltered", "{:,}".format(len(self.terms)), "-", "-"]
 
     def get_no_numbers(self):
         """
@@ -62,59 +63,57 @@ class CompressionTable:
         Given a term, once we take out all commas & periods, if it's just a string of digits, it's considered a number.
         :return: row featuring information on index without numbers.
         """
-        self.index_no_numbers = {term: postings for term, postings in self.index.items() if not term.replace(",", "").replace(".", "").isdigit()}
+        self.terms_no_numbers = [term for term in self.terms if not term.replace(",", "").replace(".", "").isdigit()]
 
-        reduction_from_previous = self.get_reduction_percentage(self.index, self.index_no_numbers)
+        reduction_from_previous = self.get_reduction_percentage(self.terms, self.terms_no_numbers)
         total_reduction = reduction_from_previous
 
-        return ["no numbers", "{:,}".format(len(self.index_no_numbers)), reduction_from_previous, total_reduction]
+        return ["no numbers", "{:,}".format(len(self.terms_no_numbers)), reduction_from_previous, total_reduction]
 
     def get_case_folding(self):
         """
         Make all terms lowercase.
         :return: row featuring information on index with all terms lowercased.
         """
-        for term in self.index_no_numbers.keys():
-            if term.casefold() not in self.index_case_folding.keys():
-                self.index_case_folding[term.casefold()] = self.index_no_numbers[term]
-            else:
-                self.index_case_folding[term.casefold()].extend(self.index_no_numbers[term])
+        for term in self.terms_no_numbers:
+            if term.casefold() not in self.terms_case_folding:
+                self.terms_case_folding.append(term.casefold())
 
-        reduction_from_previous = self.get_reduction_percentage(self.index_no_numbers, self.index_case_folding)
-        total_reduction = self.get_reduction_percentage(self.index, self.index_case_folding)
+        reduction_from_previous = self.get_reduction_percentage(self.terms_no_numbers, self.terms_case_folding)
+        total_reduction = self.get_reduction_percentage(self.terms, self.terms_case_folding)
 
-        return ["case folding", "{:,}".format(len(self.index_case_folding)), reduction_from_previous, total_reduction]
+        return ["case folding", "{:,}".format(len(self.terms_case_folding)), reduction_from_previous, total_reduction]
 
     def get_without_30_stopwords(self):
         """
         Take out 30 stopwords from the index.
         :return: row featuring information on index with 30 stopwords taken out.
         """
-        self.index_remove_30_stopwords = {term: postings for term, postings in self.index_case_folding.items() if term.lower() not in self.stopwords_30}
+        self.terms_remove_30_stopwords = [term for term in self.terms_case_folding if term.lower() not in self.stopwords_30]
 
-        reduction_from_previous = self.get_reduction_percentage(self.index_case_folding, self.index_remove_30_stopwords)
-        total_reduction = self.get_reduction_percentage(self.index, self.index_remove_30_stopwords)
+        reduction_from_previous = self.get_reduction_percentage(self.terms_case_folding, self.terms_remove_30_stopwords)
+        total_reduction = self.get_reduction_percentage(self.terms, self.terms_remove_30_stopwords)
 
-        return ["30 stopwords", "{:,}".format(len(self.index_remove_30_stopwords)), reduction_from_previous, total_reduction]
+        return ["30 stopwords", "{:,}".format(len(self.terms_remove_30_stopwords)), reduction_from_previous, total_reduction]
 
     def get_without_150_stopwords(self):
         """
         Take out 150 stopwords from the index.
         :return: row featuring information on index with 150 stopwords taken out.
         """
-        self.index_remove_150_stopwords = {term: postings for term, postings in self.index_remove_30_stopwords.items() if term.lower() not in self.stopwords_150}
+        self.terms_remove_150_stopwords = [term for term in self.terms_remove_30_stopwords if term.lower() not in self.stopwords_150]
 
-        reduction_from_previous = self.get_reduction_percentage(self.index_remove_30_stopwords, self.index_remove_150_stopwords)
-        total_reduction = self.get_reduction_percentage(self.index, self.index_remove_150_stopwords)
+        reduction_from_previous = self.get_reduction_percentage(self.terms_remove_30_stopwords, self.terms_remove_150_stopwords)
+        total_reduction = self.get_reduction_percentage(self.terms, self.terms_remove_150_stopwords)
 
-        return ["150 stopwords", "{:,}".format(len(self.index_remove_150_stopwords)), reduction_from_previous, total_reduction]
+        return ["150 stopwords", "{:,}".format(len(self.terms_remove_150_stopwords)), reduction_from_previous, total_reduction]
 
     def get_stemmed(self):
         """
         Stem all terms.
         :return: row featuring information on index with all terms stemmed.
         """
-        self.index_stem = {term: postings for term, postings in self.index_remove_150_stopwords}
+        self.terms_stemmed = {term for term in self.terms_remove_150_stopwords}
 
     @staticmethod
     def get_reduction_percentage(bigger_index, smaller_index):
